64195b4a66137102d73f5253385fceee4531497b
choclox <juancholoos2004@gmail.com>
2025-01-25 17:22:46 -0500

    "New orders app created"

---
+++ coffee_shop/orders/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Order, OrderItem
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [OrderItemInline]    
+
+admin.site.register(Order, OrderAdmin)
