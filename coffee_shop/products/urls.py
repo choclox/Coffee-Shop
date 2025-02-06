c7733d8c0b94c56318d33c86a20186c38bcc3d65
choclox <juancholoos2004@gmail.com>
2025-02-06 15:53:49 -0500

    "few changes"

--- coffee_shop/products/urls.py
+++ coffee_shop/products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProductFormView, ProductListView, ProductListAPI
 
 urlpatterns = [
+     path('', ProductListView.as_view(), name='list_products'),
     path('agregar/', ProductFormView.as_view(), name='add_product'),
-    path('listar/', ProductListView.as_view(), name='list_products'),
     path('api/', ProductListAPI.as_view(), name='list_products_api'),
 ]
